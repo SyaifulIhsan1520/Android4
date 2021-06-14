32e1aa33596da90f4cdb33ccb6c6bcf0553271b5
SyaifulIhsan1520 <65776771+SyaifulIhsan1520@users.noreply.github.com>
2021-06-14 11:42:27 +0700

    Update ServerAPI.java

--- APIVolley/app/src/main/java/com/example/apivolley/Util/ServerAPI.java
+++ APIVolley/app/src/main/java/com/example/apivolley/Util/ServerAPI.java
@@ -4,8 +4,8 @@ package com.example.apivolley.Util;
  * Setting alamat API CRUD yang telah dibuat pada CodeIgniter
  */
 public class ServerAPI {
-    public static final String URL_DATA = "http://192.168.22.10/ci/Mahasiswa/Api";
-    public static final String URL_INSERT = "http://192.168.22.10/ci/Mahasiswa/ApiInsert";
-    public static final String URL_DELETE = "http://192.168.22.10/ci/Mahasiswa/ApiDelete";
-    public static final String URL_UPDATE = "http://192.168.22.10/ci/Mahasiswa/ApiUpdate";
+    public static final String URL_DATA = "http://192.168.43.145/crud/Mahasiswa/Api";
+    public static final String URL_INSERT = "http://192.168.43.145/crud/Mahasiswa/ApiInsert";
+    public static final String URL_DELETE = "http://192.168.43.145/crud/Mahasiswa/ApiDelete";
+    public static final String URL_UPDATE = "http://192.168.43.145/crud/Mahasiswa/ApiUpdate";
 }
